089af429f3ba5aae9dfedbfc3d18f165a0259502
UnaLai <qqq323618@gmail.com>
2020-01-08 16:54:48 +0800

    [feat]:add Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <a href="#">home</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
